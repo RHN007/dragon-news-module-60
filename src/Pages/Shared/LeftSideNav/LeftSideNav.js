676d6ba23466e3a5761148930e025b57bdaef48a
RHN007 <rakibul.nayon@outlook.com>
2022-10-23 00:05:41 +0600

    adding brand carosel and others

--- src/Pages/Shared/LeftSideNav/LeftSideNav.js
+++ src/Pages/Shared/LeftSideNav/LeftSideNav.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const LeftSideNav = () => {
+    const [category, setCategory] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:6001/news-categories')
+        .then(res => res.json())
+        .then(data => setCategory(data))
+    },[])
+
+
     return (
         <div>
-            <h2>Left side nav</h2>
+                <h2>All Catagories {category.length}</h2>
+        {
+            category.map(cat => <p key={cat.id} ><Link to={`/category/${cat.id}`}>{cat.name}</Link></p>)
+        }
+
         </div>
     );
 };
